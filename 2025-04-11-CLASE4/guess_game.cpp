0f084e408c4b115520927831bea37f072bae5293
PERDOMO FORERO <juperdomof@sala12.salafis.net>
2025-04-11 08:06:42 -0500

    Agrego la condicion de estar fuera del rango pedido en el juego

---
+++ 2025-04-11-CLASE4/guess_game.cpp
@@ -0,0 +1,40 @@
+/*
+Juego de adivinar un numero
+
+Tener un numero
+Preguntar al usuario el numero
+Informar si lo adivino o no
+
+*/
+
+#include <iostream>
+
+int main(void) {
+    const int NUM = 10;
+    int guessed_number = NUM/2;
+
+    while(guessed_number != NUM){
+        std::cout <<"Adivina un numero entre 1 y 100:\n";
+        std::cin >> guessed_number;
+        //std::cout <<"Escribiste: " << guessed_number << "\n";
+
+        // validacion del rango 
+        if (guessed_number < 1){
+            std::cout << "Estas fuera del rango. ¡Se termina el juego!\n";
+            break;
+        } else if (guessed_number > 100){
+            std::cout << "Estas fuera del rango. ¡Se termina el juego!\n";
+            break;
+        }
+
+        if (guessed_number == NUM){
+            std::cout <<"GANASTE!!!\n";
+        } else if (guessed_number > NUM){
+            std::cout <<"Te pasaste\n";
+        } else { // es menor
+            std::cout <<"Te falta\n";
+    }
+    }
+
+    return 0;
+}
