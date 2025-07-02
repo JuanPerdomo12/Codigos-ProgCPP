6b87fa5a61c35be4b933385dd0635b6bd752181f
PERDOMO FORERO <juperdomof@sala12.salafis.net>
2025-07-02 08:54:57 -0500

    Agrego los codigos de OOP

---
+++ 2025-07-02-CLASE21/boundary.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <valarray>
+#include <iostream>
+
+class Boundary{
+  double RMAX_{0.0}, EN_{0.0}, ET_{0.0}; // RMAX: radius of the sphere, EN: normal restitution coefficient, ET: tangential rest. coeff
+  std::valarray<double> C_{0.0, 0.0, 0.0}; // Sphere center
+
+  public:
+    Boundary(double RMAX, double CX, double CY, double CZ, double EN, double ET) {
+      RMAX_ = RMAX;
+      C_[0] = CX;
+      C_[1] = CY;
+      C_[2] = CZ;
+      EN_ = EN;
+      ET_ = ET;
+    }
+
+    template <class particle_array_t>
+    void apply(particle_array_t & parray) {
+      // applySphericalConstraint
+      for (auto & p : parray) {
+        std::valarray<double> Rprime = p.R - C_;
+        double rprime = std::sqrt((Rprime*Rprime).sum());
+        double delta = rprime + p.rad - RMAX_;
+        if (delta > 0) {
+            std::valarray<double> N = Rprime/rprime;
+            p.R = p.R - delta*N;
+            p.V = p.V - (1 + EN_) * (p.V*N).sum() * N;
+        }
+      }
+    }
+};
